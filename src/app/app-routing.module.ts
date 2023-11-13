14be7104a18e3929b6503b37c74b382a4dd90c86
Andy Chung <andy6756@gmail.com>
2023-11-14 00:56:20 +0800

    add routing setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'layout',
+    component: LayoutComponent
+  },
+  {
+    path: '',
+    redirectTo: 'layout',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
